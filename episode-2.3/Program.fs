7475bfec04fb09bc62c71531c5580db3bab04fd3
Daniel Weller <daniel.weller@softwarepark.cc>
2022-03-31 12:08:45 +0200

    nicer model evaluation

--- episode-2.3/Program.fs
+++ episode-2.3/Program.fs
@@ -4,26 +4,36 @@ module TransportTycoon.Main
 let main argv =
     let trainingData = TransportTycoon.Model.ParseCsv.getTrainingData "s02e03_train.csv"
     let testData = TransportTycoon.Validation.ParseCsv.getTestData "s02e03_test.csv"
-    
-    let averageSpeedModel = TransportTycoon.Model.AverageSpeedModel.getModel trainingData
-    let averageSpeedModelError =
-        TransportTycoon.Validation.MeanSquareError.modelError averageSpeedModel testData
 
+    let averageSpeedModel = TransportTycoon.Model.AverageSpeedModel.getModel trainingData
     let linearModel = TransportTycoon.Model.LinearRegressionModel.getModel trainingData
-    let linearModelError =
-        TransportTycoon.Validation.MeanSquareError.modelError linearModel testData
-    
-    System.Console.WriteLine($"AverageSpeedModel MSE: {averageSpeedModelError}")
-    System.Console.WriteLine($"LinearModel MSE: {linearModelError}")
 
     let polynomialOrders = [2; 3; 4; 5]
     
-    polynomialOrders
-    |> Seq.iter (fun order ->
-        let model = TransportTycoon.Model.PolynomialRegressionModel.getModel order trainingData
-        let error =
-            TransportTycoon.Validation.MeanSquareError.modelError model testData
-        System.Console.WriteLine($"PolynomialModel (order {order}) MSE: {error}")
-        )
-    
+    let polynomialModels =
+        polynomialOrders
+        |> Seq.map (fun order ->
+            let model = TransportTycoon.Model.PolynomialRegressionModel.getModel order trainingData
+            $"Polynomial Regression (order {order})", model)
+
+    let models =
+        [ "Average Speed", averageSpeedModel
+          "Linear Regression", linearModel ]
+        |> Seq.append polynomialModels
+
+    let evaluatedModels =
+        models
+        |> Seq.map( fun (name, model) ->
+            (name, model, TransportTycoon.Validation.MeanSquareError.modelError model testData))
+
+    evaluatedModels
+    |> Seq.iter(fun (name, model, error) ->
+        System.Console.WriteLine($"{name} MSE: {error}"))
+
+    let bestModel, _, _ =
+        evaluatedModels
+        |> Seq.minBy (fun (name, model, error) -> error)
+
+    System.Console.WriteLine($"Best Model: {bestModel}")
+
     0
